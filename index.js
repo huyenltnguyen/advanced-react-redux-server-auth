e78c168828081abf924215b16167c544726a8599
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-06-15 05:51:31 +0700

    set header

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ mongoose.connect('mongodb://localhost/auth');
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
+
+  next();
+});
+
 router(app);
 
 // Server setup
