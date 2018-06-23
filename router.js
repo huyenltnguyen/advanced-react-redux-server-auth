3168d41708b437adc98968c9130183fb4ebb0ac7
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-06-23 18:43:43 +0700

    send JSON response if Passport auth fails

--- router.js
+++ router.js
@@ -3,14 +3,19 @@ import passport from 'passport';
 import passportService from './services/passport';
 
 const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', { session: false });
+const requireSignin = passport.authenticate('local', { session: false, failWithError: true });
 
 export default (app) => {
   app.get('/', requireAuth, (req, res) => {
     res.json({ message: 'hi' });
   });
 
-  app.post('/signin', requireSignin, signinAuth);
+  // handle failed Passport.js authentication without a custom callback
+  // REF: https://stackoverflow.com/questions/15388206/sending-back-a-json-response-when-failing-passport-js-authentication
+  // REF: https://github.com/jaredhanson/passport/issues/126#issuecomment-32333163
+  app.post('/signin', requireSignin, signinAuth, (err, req, res, next) => {
+    return res.json({ message: 'Invalid email or password.' });
+  });
 
   app.post('/signup', signupAuth);
 };
